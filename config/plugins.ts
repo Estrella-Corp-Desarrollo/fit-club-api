5df28c4ab821b5058a3fc9f989233d45667a20b2
isra-estrella <ti@heladosestrella.com>
2026-06-19 16:47:31 -0700

    storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -18,5 +18,24 @@ export default ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('DO_SPACES_ACCESS_KEY'),
+        secretAccessKey: env('DO_SPACES_SECRET_KEY'),
+        region: env('DO_SPACES_REGION'), // ej: 'nyc3', 'sgp1', 'ams3'
+        endpoint: env('DO_SPACES_ENDPOINT'), // ej: 'https://nyc3.digitaloceanspaces.com'
+        params: {
+          Bucket: env('DO_SPACES_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   // ...
 });
